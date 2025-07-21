ecf229662052c62fdf2e7562a860a90de795dec7
shohan12727 <aishohan001@gmail.com>
2025-07-21 13:16:30 +0600

    first commit

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+const greet = (name) => {
+    return `Hello, ${name}!`;
+};
+console.log(greet("Shohan"));
+let userName = 'Shohan';
+console.log(userName);
+const age = 40;
+console.log(age);
+const univarsityName = 'North South Univarsity';
+console.log(univarsityName);
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(12, 50));
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+const greet = (name: string): string => {
+  return `Hello, ${name}!`;
+};
+
+console.log(greet("Shohan"));
+
+
+let userName: string = 'Shohan';
+console.log(userName);
+
+
+const age = 40;
+console.log(age);
+
+const univarsityName = 'North South Univarsity';
+console.log(univarsityName);
+
+const sum = (a:number ,b:number) => {
+  return a+b ;
+}
+console.log( sum(12,50));
+
+
+
