58c42f9884fe38ebeafc5d8fc162d56ba772d34d
shohan12727 <aishohan001@gmail.com>
2025-07-27 16:39:46 +0600

    understanding class in ts ongoing

--- TS-5/index.js
+++ TS-5/index.js
@@ -15,4 +15,64 @@ var NameValue = /** @class */ (function () {
 }());
 var bookBox = new NameValue("Books");
 bookBox.setValue("Harry potter");
-console.log(bookBox.toString());
+var Harry = {
+    name: "Harry potter",
+    age: 32
+};
+var Ronaldo = {
+    name: "Cristiano Ronaldo",
+    age: 39,
+    club: "Al Nasser",
+    isGOAT: true
+};
+var messi = {
+    name: "Leo Messi",
+    age: 37,
+    club: "Pink Miami",
+    isGOAT: false
+};
+console.log(messi);
+console.log(Ronaldo);
+var Patient = /** @class */ (function () {
+    function Patient(id, name, age, disease) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.disease = disease;
+    }
+    Patient.prototype.getDetails = function () {
+        return "Details of the patient: ".concat(this.name, ", ").concat(this.age, ", ").concat(this.disease);
+    };
+    return Patient;
+}());
+var patient1 = new Patient(1, "Rajesh", 20, "Cold");
+console.log(patient1);
+var BankAccount = /** @class */ (function () {
+    function BankAccount(accountHolder, initialBalance) {
+        this.accountHolder = accountHolder;
+        this._balance = initialBalance;
+    }
+    Object.defineProperty(BankAccount.prototype, "balance", {
+        get: function () {
+            return this._balance;
+        },
+        set: function (ammount) {
+            if (ammount > 0) {
+                console.log("Balance can't be negative");
+                return;
+            }
+            this._balance = ammount;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    BankAccount.prototype.displayInfo = function () {
+        console.log("".concat(this.accountHolder, "'s balance: ").concat(this._balance));
+    };
+    return BankAccount;
+}());
+var acc1 = new BankAccount("Shohan", 1000);
+console.log(acc1);
+console.log(acc1.balance);
+acc1.balance = -82585;
+console.log(acc1.balance);
--- TS-5/index.ts
+++ TS-5/index.ts
@@ -17,4 +17,113 @@ class NameValue<T> {
 let bookBox = new NameValue<string>("Books");
 bookBox.setValue("Harry potter");
 
-console.log(bookBox.toString());
+// console.log(bookBox.toString());
+
+// ----------------------------------------------
+
+
+
+interface Person {
+  name: string,
+  age: number
+}
+
+const Harry: Person = {
+  name: "Harry potter",
+  age: 32
+}
+// console.log(Harry);
+
+
+interface FootballPlayer {
+  name: string,
+  age: number,
+  club: string, 
+  isGOAT: boolean
+}
+const Ronaldo: FootballPlayer = {
+  name: "Cristiano Ronaldo",
+  age: 39,
+  club: "Al Nasser",
+  isGOAT: true
+}
+
+const messi: FootballPlayer = {
+  name: "Leo Messi",
+  age: 37,
+  club: "Pink Miami",
+  isGOAT: false
+}
+console.log(messi);
+console.log(Ronaldo);
+
+
+type Car = {
+   brand: string,
+   year: number,
+   isElectric: boolean
+}
+
+type carKey = keyof Car;
+
+
+class Patient {
+  id: number;
+  name: string;
+  age: number;
+  disease: string
+
+  constructor(id: number, name: string, age: number, disease: string){
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.disease = disease;
+  }
+
+  getDetails () {
+    return `Details of the patient: ${this.name}, ${this.age}, ${this.disease}`
+  }
+}
+
+const patient1 = new Patient(1,"Rajesh",20,"Cold");
+console.log(patient1);
+
+
+
+class BankAccount {
+  public accountHolder: string;
+  private _balance: number;
+ 
+
+  constructor(accountHolder: string, initialBalance: number) {
+    this.accountHolder = accountHolder;
+    this._balance = initialBalance;
+  }
+ 
+  public get balance(): number{
+    return this._balance;
+  }
+
+  public set balance(ammount: number): number {
+    if(ammount > 0){
+      console.log("Balance can't be negative");
+      return;
+    }
+    this._balance = ammount
+  }
+
+  public displayInfo(): void {
+    console.log(`${this.accountHolder}'s balance: ${this._balance}`);
+  }
+}
+
+const acc1 = new BankAccount ("Shohan",1000);
+
+console.log(acc1);
+
+console.log(acc1.balance);
+
+acc1.balance = -82585;
+console.log(acc1.balance);
+
+
