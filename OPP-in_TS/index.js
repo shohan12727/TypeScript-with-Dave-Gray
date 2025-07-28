4931e139dd8abaed9ae71a3ab7ad89ac73761055
shohan12727 <aishohan001@gmail.com>
2025-07-28 16:31:52 +0600

    code added

--- OPP-in_TS/index.js
+++ OPP-in_TS/index.js
@@ -20,8 +20,48 @@ var User = /** @class */ (function () {
 }());
 // create user 
 var user1 = new User("Shohan", "rajesh1122");
-console.log(user1.authenticate("1234"));
-console.log(user1.authenticate("rajesh1122"));
+// console.log(user1.authenticate("1234"));
+// console.log(user1.authenticate("rajesh1122"));
 user1.changePassword("shohan");
-console.log(user1.authenticate("rajesh1122"));
-console.log(user1.authenticate("shohan"));
+// console.log(user1.authenticate("rajesh1122"));
+// console.log(user1.authenticate("shohan"));
+function logMassage(message) {
+    console.log(message);
+}
+// console.log(logMassage());
+// so the void function can't return anything it can only executes any opertaion but can not return
+var LibraryBook = /** @class */ (function () {
+    function LibraryBook(title, author, isCheckedOut) {
+        this.title = title;
+        this.author = author;
+        this.isCheckedOut = false; //initially available
+    }
+    LibraryBook.prototype.checkOut = function () {
+        if (!this.isCheckedOut) {
+            this.isCheckedOut = true;
+            console.log("you have already check out \"".concat(this.title, "\" by ").concat(this.author));
+            return true;
+        }
+        else {
+            console.log("Sorry, \"".concat(this.title, "\" is already checked out"));
+            return false;
+        }
+    };
+    // method to return book 
+    LibraryBook.prototype.returnBook = function () {
+        if (this.isCheckedOut) {
+            this.isCheckedOut = false;
+            console.log("You have already returned \"".concat(this.title, "\", Thank you!"));
+        }
+        else {
+            console.log("".concat(this.title, " was not checked out"));
+        }
+    };
+    LibraryBook.prototype.getStatus = function () {
+        return this.isCheckedOut ? "Checked out" : "Available";
+    };
+    return LibraryBook;
+}());
+var book = new LibraryBook("1991", "George Orwell", false);
+console.log(book.getStatus());
+book.checkOut();
--- OPP-in_TS/index.ts
+++ OPP-in_TS/index.ts
@@ -22,12 +22,12 @@ class User {
 // create user 
 const user1 = new User("Shohan", "rajesh1122");
 
-console.log(user1.authenticate("1234"));
-console.log(user1.authenticate("rajesh1122"));
+// console.log(user1.authenticate("1234"));
+// console.log(user1.authenticate("rajesh1122"));
 
 user1.changePassword("shohan");
-console.log(user1.authenticate("rajesh1122"));
-console.log(user1.authenticate("shohan"));
+// console.log(user1.authenticate("rajesh1122"));
+// console.log(user1.authenticate("shohan"));
 
 
 
@@ -36,7 +36,65 @@ console.log(user1.authenticate("shohan"));
 
 
 
+function logMassage(message: string): void {
+  console.log(message);
+}
 
+// console.log(logMassage());
+
+
+// so the void function can't return anything it can only executes any opertaion but can not return
+
+
+
+
+
+
+class LibraryBook {
+  private title: string;
+  private author: string;
+  private isCheckedOut: boolean;
+
+
+  constructor(title: string, author: string, isCheckedOut: boolean) {
+    this.title = title;
+    this.author = author;
+    this.isCheckedOut = false; //initially available
+  }
+
+  checkOut(): boolean {
+    if (!this.isCheckedOut) {
+      this.isCheckedOut = true;
+      console.log(`you have already check out "${this.title}" by ${this.author}`);
+      return true;
+    } else {
+      console.log(`Sorry, "${this.title}" is already checked out`);
+      return false;
+    }
+  }
+
+
+  // method to return book 
+  returnBook():void {
+    if (this.isCheckedOut){
+      this.isCheckedOut = false;
+      console.log(`You have already returned "${this.title}", Thank you!`);
+    } else {
+      console.log(`${this.title} was not checked out`);
+    }
+  }
+
+  getStatus(): string {
+    return this.isCheckedOut ? "Checked out" : "Available"
+  }
+}
+
+const book = new LibraryBook("1991", "George Orwell", false);
+
+
+console.log(book.getStatus());
+
+book.checkOut();
 
 
 
