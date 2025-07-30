35a36018b036fe4658c563308d44f72c35494ec5
shohan12727 <aishohan001@gmail.com>
2025-07-30 12:08:35 +0600

    lc problem

--- Encapsulation_review/index.js
+++ Encapsulation_review/index.js
@@ -1,3 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // Encapsulation means hiding the internal details of how something works and only showing what is necessary to outside 
 var User = /** @class */ (function () {
     function User(userName, password) {
@@ -19,8 +21,47 @@ var User = /** @class */ (function () {
     return User;
 }());
 var user1 = new User("Pal Sagar", "rp1122");
-console.log(user1.getUserName());
-console.log(user1.authenticate('rp1122'));
+// console.log(user1.getUserName());
+// console.log(user1.authenticate('rp1122'));
 user1.changePassword("Shohan");
 // here password has been changed 
-console.log(user1.authenticate('rp1122'));
+// console.log(user1.authenticate('rp1122'));
+var friends = [
+    "Ayaan",
+    "Nira",
+    "Farhan",
+    "Sadia",
+    "Rifat",
+    "Mahi",
+    "Tamim",
+    "Lamia",
+    "Rakib",
+    "Tanha",
+    "Sajid",
+    "Oni",
+    "Partho",
+    "Jui",
+    "Badhon",
+    "Reem",
+    "Rasel",
+    "Shaila",
+    "Noman",
+    "Elita"
+];
+function getFirstLeterOfArray(arr) {
+    for (var i = 0; i < arr.length; i += 1) {
+        var firstLetter = arr[i][0];
+        console.log("First letter of ".concat(arr[i], " is: ").concat(firstLetter));
+    }
+}
+getFirstLeterOfArray(friends);
+function getLastLeterOfArray(arr) {
+    for (var i = 0; i < arr.length; i += 1) {
+        var lastLetter = arr[i][arr[i].length - 1];
+        console.log("last letter of ".concat(arr[i], " is: ").concat(lastLetter));
+    }
+}
+getLastLeterOfArray(friends);
+var name = "Shohan";
+console.log(name.length);
+// writing this i can tell typescript that  this file is a module 
--- Encapsulation_review/index.ts
+++ Encapsulation_review/index.ts
@@ -28,10 +28,64 @@ class User {
 
 const user1 = new User("Pal Sagar", "rp1122");
 
-console.log(user1.getUserName());
+// console.log(user1.getUserName());
 
-console.log(user1.authenticate('rp1122'));
+// console.log(user1.authenticate('rp1122'));
 
 user1.changePassword("Shohan");
 // here password has been changed 
-console.log(user1.authenticate('rp1122'));
+// console.log(user1.authenticate('rp1122'));
+
+
+
+const friends: string[] = [
+  "Ayaan",
+  "Nira",
+  "Farhan",
+  "Sadia",
+  "Rifat",
+  "Mahi",
+  "Tamim",
+  "Lamia",
+  "Rakib",
+  "Tanha",
+  "Sajid",
+  "Oni",
+  "Partho",
+  "Jui",
+  "Badhon",
+  "Reem",
+  "Rasel",
+  "Shaila",
+  "Noman",
+  "Elita"
+];
+
+
+function getFirstLeterOfArray(arr: string[]): void {
+  for (let i = 0; i < arr.length; i += 1) {
+    const firstLetter = arr[i][0];
+    console.log(`First letter of ${arr[i]} is: ${firstLetter}`);
+
+  }
+}
+getFirstLeterOfArray(friends)
+
+function getLastLeterOfArray(arr: string[]): void {
+  for (let i = 0; i < arr.length; i += 1) {
+    const lastLetter = arr[i][arr[i].length - 1];
+    console.log(`last letter of ${arr[i]} is: ${lastLetter}`);
+
+  }
+}
+getLastLeterOfArray(friends);
+
+const name: string = "Shohan";
+console.log(name.length);
+
+
+
+
+export {}
+
+// writing this i can tell typescript that  this file is a module 
